7ab5a3a19ef8836cc22cb86b07e04edf2557009f
jacquemmoz <julien.jacquemmoz.auditeur@lecnam.net>
2026-06-14 09:00:27 +0000

    Reapply "feat(feuilles.py):mise en conformité SRP ; test non régression ok"
    This reverts commit bac004275fda4efb5f7e76f6f43e56bcdcc3785b.

--- README.md
+++ README.md
@@ -65,42 +65,115 @@ Ces opérations sont décrites et exécutables dans le notebook `exploration.ipy
 
 # Traitements envisagés et maquette
 
->Dossier 'maquettes' : évolution des maquettes durant l'avacement du projet, au format *excalidraw* et *png*.
+La seule donnée quantitative est la nombre de validations.
+Les variables catégorielles sont : date, station et categorie titre. La date est par ailleurs une variable ordinale.
+On peut donc aisément envisager de stratifier le **nombre de validations par titre de transport** (barres empilées) **selon la date ou la station**.
 
+!["Première maquette du tableau de bord envisagé"](img/maquette1.png)
+*Première maquette du tableau de bord envisagé*
 
+> On réalise des simulations à partir du jeu de données sur Excel avec des *graphiques croisés dynamiques*.
 
+On souhaiterait indiquer les stations sur un plan de métro, les points étant dynamiquement placés. Le jeu de données *Coordonnées des stations sur le plan schématique RATP* fourni par data.gouv.fr le permettrait ([https://www.data.gouv.fr/datasets/coordonnees-des-stations-sur-le-plan-schematique-ratp-ratp]).
 
-### Traitements
+!["Deuxième maquette du tableau de bord envisagé"](img/maquette2.png)
+*Deuxième maquette du tableau de bord envisagé*
 
-- concaténation 110 (==> transporteur 100)
-- 365j : OK, continuité ok
-- 319 arrêts différents
-- code_stif_arret, ida : "doublons"
-	- valeurs aberrantes Mairie de St-Ouen, Maison-Blanche
-- catégorie_titre : 
-	- Forfaits courts
-	- Autres titres
-	- Contrat Solidarité Transport
-	- Forfait Navigo
-	- Imagine R
-	- Amethyste
-	- NON DEFINI
-	- Contrat Solidarite Transport
-- contrat SolidartiE SolidaritÉ
-- ANTONY ?
+Il n'est malheureusement pas possible de télécharger ce jeu de données, forçant à abandonner cette idée.
 
-# Indicateurs
+Les simulations montrent, logiquement, la baisse du nombre de validations les week-ends :
 
-- agréger titres "sociaux" ?
-- forfaits spéciaux ? prévalence ? Par date.
-- inconnu/anomalies ? prévalence ?
+!["Fréquentation des stations de métro par jour"](img/GCD.png)
+*Fréquentation des stations de métro par jour : on voit nettement la baisse de fréquentation les week-ends.*
 
-- 3 dimensions : stations (lignes), jours, titre
-- géographie ?
+Ce fait logique n'apporte aucune information, par ailleurs la représentation des données avec une granulométrie quotidienne génère un graphe trop chargé. On retiendra donc une **granulométrie hebdomadaire**, qui résoudra ces deux problèmes.
 
-- série temporelle :
-	- par jour : effet w-e, + été
-	- par semaine : été
-	- forfaits courts vs Navigo/ImaginR
+Le nombre important de stations (319) génère également un graphe trop chargé. On présentera donc un **échantillon de 50 stations tirées *dynamiquement* au sort**.
+
+**Enfin, on relève des différences de répartition des types de titres de transport selon les stations, que nous devons mettre en évidence sur le tableau de bord.** Nous étudierons les **titres de transports attribus sur critères sociaux (agrégeant "Amethyste" et "Contrat Solidarité Transport")** et les **Forfaits courts**.
+
+> Titres de transports attribus sur critères sociaux = Amethyste + Contrat Solidarité Transport
+
+La fréquentation temporelle sera par défaut présentée pour le total des stations, une liste déroulante permettra de sélectionner une station précise.
+
+!["Maquette définitive du tableau de bord](img/maquette3.png)
+*Maquette définitive du tableau de bord*
+
+# Mise en oeuvre effective des traitements
+
+Le fichier Excel calculant et affichant le tableau de bord est généré par des scripts Python, à partit des données traitées `data_traitee.xlsx` générées par le notebook `exploration.ipynb`.
+
+## Organisation des scripts Python
+
+> **`uv run metro`**
+
+Les données traitées sont passées au script `main.py`, qui les recopie dans la feuille `DATA` du classeur de sortie, et orchestre sa création. Il appelle successivement les scripts `feuilles.py`, `graphes1.py` et `graphes2.py`, responsables respectivement de la génération des feuilles de calcul et des deux feuilles constituant le tableau de bord. Le script `forme.py` contient les éléments de mise en forme des graphes, afin de séparer, dans le projet, la forme du contenu. Le tableau de bord est généré sous forme d'un fchier `sortie.xlsx`.
+
+**Les différents fichiers contiennent des commentaires explicatifs, et les focntions appelées sont dotées de *docstrings*.**
+
+## Organisation du classeur contenant le tableau de bord
+
+*Dans les paragraphes suivants, les formules Excel seront indiquées en français, telles qu'elles apparaitront dans le fichier généré. Dans les scripts, elles sont traitées en anglais, conformément aux spécifications du package* Openpyxl *, qui est utilisé pour traiter les fichiers Excel.*
+
+### Feuille `DATA`
+
+Elle correspond à la copie des données traitées, auxquelles sont ajoutées une colonne correspondant au numéro de la semaine : `=NO.SEMAINE()`.
+
+### Feuille `modalites`
+
+Elle contient une colonne présentant l'ensemble des stations de métro, suivi de la valeur "toutes". Elle sert de référence à la liste déroulante de sélection du nom de station dans la feuille `Graphe1`.
+
+*Elle contient également la liste des différents types de titres de transport, qui n'est pas utilisée mais est présente pour permettre de futures évolutions du tableau de bord.*
+
+*Cette feuille est statique, générée directement en Python, donc sans formules Excel. Le recours à la formule =UNIQUE() e été testé (avec calcul de la plage dynamique de l'ArrayFormula) mais non retenu : voir #6 .*
+
+### Feuille `jours`
+
+*Cette feuille a été calculée lors du prototypage du tableau de bord, avant que l'on décide d'une granulométrie hebdomadaire. Elle est maintenue dans le classeur pour de futuregit branchs évolutions. Elle permet également d'illustrer la logique des calculs dans le classeur.*
+
+Cette feuille pivote des données de `DATA` selon la date des jours, en ajoutant les nombres de validations, en utilisant un ensemble de formule de la forme :
+
+`=SOMME.SI.ENS(DATA!$D:$D;DATA!$A:$A;$A2;DATA!$B:$B;SI(station="toutes";"*";station);DATA!$C:$C;B$1)`
+
+Par exemple pour la cellule B2 :
+
+- somme selon la colonne $D:$D `nb_vald`,
+- pour la date égale à la valeur contenue dans l'entête de ligne (dans l'exemple cellule $A2),
+- pour la station indiquée dans la valeur de la cellule nommée 'station' (cellule avec une liste déroulante présente dans la feuille `Graphes1`), en traitant le cas où l'affichage de toutes les stations est demandé, 
+- pour le type de titre de transport égal à la valeur contenue dans l'entête de la colonne (dans l'exemple cellule B$1).
+
+### Feuille `semaines`
+
+Cette feuille pivote les données de `DATA` selon la même logique que la feuille `jours`, mais selon le numéro de la semaine. Le premier critère de la somme conditionnelle sera donc non plus la colonne `jour`, mais la colonne `semaine` :
+
+`=SOMME.SI.ENS(DATA!$D:$D;DATA!$E:$E;$A2;DATA!$B:$B;SI(station="toutes";"*";station);DATA!$C:$C;B$1)`
+
+### Feuille `stations`
+
+Ici le pivot est fait non plus selon une date, mais selon la station (`libelle_arret`, colonne B), indiquée en entête de ligne :
+
+`=SOMME.SI.ENS(DATA!$D:$D;DATA!$B:$B;$A2;DATA!$C:$C;B$1)`
+
+Des colonnes supplémentaires sont calculées :
+- `aleatoire=ALEA()` : valeur aléatoire qui servira pour l'écantillonnage dans l'onglet `echantillon`,
+- `prop_social=(B2+D2)/SOMME(B2:H2)` : proportion de titres de transports "sociaux" ;
+- `prop_court==(F2+D2)/SOMME(B2:H2)` : proportion de titres de transport courts.
+
+Ces deux dernières colonnes permmettent de générer les plages qui permettront la représentation des stations les plus et les moins fréquentées par les détenteurs de titres de transport "sociaux" et courts. Ces plages sont matérialisées par l'aggrégation des lignes correspondant aux 10 plus fortes et plus faibles valeurs des colonnes de proportions, grâce aux formules :
+
+`=PRENDRE(TRIER($A$2:$J$319;10;-1;);10)` et
+`=PRENDRE(TRIER($A$2:$J$319;10;-1;);-10)`
+
+Sont ajoutées à ces plages des colonnes `top` et `flop` reprenant les valeurs de la colonne proportion qui appartiendront respectivement aux séries des stations les plus et les moins fréquentées.
+
+### Feuille `echantillon`
+
+Cette feuille contient un tirage aléatoire des fréquentations de 50 stations issues de la feuille `stations` selon les 50 plus faibles valeurs contenue dans la colonne `aleatoire`) :
+
+`=PRENDRE(TRIERPAR(stations!A:H;stations!I:I);50)`
+
+# Perpectives
+
+- forfaits courts vs Navigo/ImaginR
 	
 - titre spécial 21/6 (début année : titres papier ?)
--- src/metro/feuilles.py
+++ src/metro/feuilles.py
@@ -1,4 +1,4 @@
-"""Calcule les feuilles du classeur"""
+"""Orchestre le calcul des feuilles du classeur"""
 
 import datetime
 from openpyxl import Workbook
@@ -7,14 +7,14 @@ from openpyxl.worksheet.formula import ArrayFormula
 from openpyxl.utils import get_column_letter
 
 
-def unique(ensemble: tuple | list, suppr_entete=True) -> list:     # type de plage à mettre
+def unique(ensemble: tuple | list, suppr_entete=True) -> list:
     """
     Retourne la liste triée des éléments uniques d'un ensemble
     :param ensemble:
     :param suppr_entete: True pour supprimer la première valeur de l'ensemble
     """
     ensemble = list(ensemble)
-    if suppr_entete: 
+    if suppr_entete:
         ensemble.pop(0)
     uniques = set()
     for element in ensemble:
@@ -24,10 +24,10 @@ def unique(ensemble: tuple | list, suppr_entete=True) -> list:     # type de pla
     return uniques
 
 
-def topflop(feuille: Worksheet, debut:int) -> None:
+def topflop(feuille: Worksheet, debut: int) -> None:
     """
-    Procédure qui renseigne les colonnes 'top' et 'flop' de la feuille 'stations' (voir documentation)
-    et les met au format pourcentage
+    Procédure qui renseigne les colonnes 'top' et 'flop' de la feuille 'stations'
+    (voir documentation) et les met au format pourcentage
     :param feuille: feuille 'stations'
     :param debut: numéro de la première colonne ('top')
     """
@@ -37,99 +37,152 @@ def topflop(feuille: Worksheet, debut:int) -> None:
         feuille.cell(j, debut).number_format = "0.00%"
     feuille.cell(1, debut+1).value = "flop"
     for j in range(12, 22):
-        feuille.cell(j, debut+1).value =  "=" + get_column_letter(debut-1) + str(j)
+        feuille.cell(j, debut+1).value = "=" + get_column_letter(debut-1) + str(j)
         feuille.cell(j, debut+1).number_format = "0.00%"
 
 
-def feuilles(classeur: Workbook):
-    # 1. feuille 'DATA' : ajout d'une colonne avec le numéro de la semaine
+def data(classeur: Workbook):
+    # modifie la feuille 'DATA' : ajoute une colonne avec le numéro de la semaine
     data = classeur['DATA']
     data["E1"].value = "semaine"
     for ligne in range(2, data.max_row+1):  # enumerate ?
         data.cell(row=ligne, column=5).value = "=WEEKNUM($A{l})".format(l=ligne)
 
-    # 2. feuille 'modalites'
+
+def modalites(classeur: Workbook):
+    """
+    crée la feuille 'modalites'
+    """
     modalites = classeur.create_sheet("modalites")
-    ## stations
-    liste_stations = unique(data["B"])
+
+    # # stations
     for i, valeur in enumerate(liste_stations, start=1):
         modalites.cell(row=i, column=1, value=valeur)
-    # la valeur/modalité "toutes" sera ajoutée pour permettre les calculs portant sur l'ensemble des stations
+    # la valeur/modalité "toutes" sera ajoutée pour permettre les calculs
+    # portant sur l'ensemble des stations
     modalites.append(("toutes",))
 
-    ## titres de transport
-    liste_titres = unique(data["C"])
+    # # titres de transport
     for i, valeur in enumerate(liste_titres, start=1):
         modalites.cell(row=i, column=2, value=valeur)
 
-    # 3. feuille 'jours'
+
+def jours(classeur: Workbook):
+    """
+    crée la feuille 'jours'
+    """
     jours = classeur.create_sheet("jours")
-    ## entêtes
+
+    # # entêtes
     jours.append(["jour",] + liste_titres)
-    ## valeurs : jour + les comptes des différents titres de transport
-    ## =SOMME.SI.ENS(DATA!$D:$D;DATA!$A:$A;$A{l};DATA!$B:$B;SI(station="toutes";"*";station);DATA!$C:$C;B$1)
-    ## la cellule 'station' (nom défini) est définie dans 'graphes1.py'
-    ## avec {l}=numéro de la ligne et {c}=lettre de la colonne (B à H)
+
+    # # valeurs : jour + les comptes des différents titres de transport
+    # # =SOMME.SI.ENS(DATA!$D:$D;DATA!$A:$A;$A{l};DATA!$B:$B;SI(station="toutes";"*";station);DATA!$C:$C;B$1)
+    # # la cellule 'station' (nom défini) est définie dans 'graphes1.py'
+    # # avec {l}=numéro de la ligne et {c}=lettre de la colonne (B à H)
     formule_jours = "=SUMIFS(DATA!$D:$D,DATA!$A:$A,$A{l},DATA!$B:$B,IF(station=\"toutes\",\"*\",station),DATA!$C:$C,{c}$1)"
     for i in range(0, 365):
-        ligne:list = [datetime.date(2025, 1, 1) + datetime.timedelta(i)]
+        ligne: list = [datetime.date(2025, 1, 1) + datetime.timedelta(i)]
         for colonne in ("B", "C", "D", "E", "F", "G", "H"):
             ligne.append(formule_jours.format(l=i+2, c=colonne))
         jours.append(ligne)
 
-    # 4. feuille 'semaines'
+
+def semaines(classeur: Workbook):
+    # crée la feuille 'semaines'
     semaines = classeur.create_sheet("semaines")
-    ## entêtes
+
+    # # entêtes
     semaines.append(["semaine",] + liste_titres)
-    ## valeurs : numéro de semaine + les comptes des différents titres de transport
-    ## formule_semaines = formule_jours en remplaçant DATA!$A:$A,$A{l} par DATA!$E:$E,$A{l}
+
+    # # valeurs : numéro de semaine + les comptes des différents titres de transport
+    # # formule_semaines = formule_jours en remplaçant DATA!$A:$A,$A{l} par DATA!$E:$E,$A{l}
     formule_semaines = \
         "=SUMIFS(DATA!$D:$D,DATA!$E:$E,$A{l},DATA!$B:$B,IF(station=\"toutes\",\"*\",station),DATA!$C:$C,{c}$1)"
     for i in range(1, 54):
-        ligne:list = [i]
+        ligne: list = [i]
         for colonne in ("B", "C", "D", "E", "F", "G", "H"):
             ligne.append(formule_semaines.format(l=i+1, c=colonne))
         semaines.append(ligne)
 
-    # 5. feuille 'stations'
+
+def stations(classeur: Workbook):
+    """
+    crée la feuille 'stations'
+    """
     stations = classeur.create_sheet("stations")
-    ## entêtes
+
+    # # entêtes
     stations.append(["station",] + liste_titres + ["aleatoire", "prop_social", "prop_court"])
-    ## valeurs : nom de station + les comptes des différents titres de transport +...
-    ## =SOMME.SI.ENS(DATA!$D:$D;DATA!$B:$B;$A{l};DATA!$C:$C;{c}$1)
+
+    # # valeurs : nom de station + les comptes des différents titres de transport +...
+    # # =SOMME.SI.ENS(DATA!$D:$D;DATA!$B:$B;$A{l};DATA!$C:$C;{c}$1)
     formule_stations = "=SUMIFS(DATA!$D:$D,DATA!$B:$B,$A{l},DATA!$C:$C,{c}$1)"
     for i in range(len(liste_stations)):
         ligne = [liste_stations[i],]
         for colonne in ("B", "C", "D", "E", "F", "G", "H"):
             ligne.append(formule_stations.format(l=i+2, c=colonne))
-        ## ...+ valeur aléatoire qui servira pour l'échantillonnage dans l'onglet suivant +...
+        # # ...+ valeur aléatoire qui servira pour l'échantillonnage dans l'onglet suivant +...
         ligne.append("=RAND()")
-        ## ...+ proportion de titres de transport 'sociaux' ("Amethyste" et "Contrat Solidarité Transport") et courts
+        # # ...+ proportion de titres de transport 'sociaux'
+        # # ("Amethyste" et "Contrat Solidarité Transport") et courts
         ligne.append("=(B{l}+D{l})/SUM(B{l}:H{l})".format(l=i+2))
         ligne.append("=(F{l}+D{l})/SUM(B{l}:H{l})".format(l=i+2))
         stations.append(ligne)
 
-    ## chiffres pour les 'titres de transport sociaux'
-    ## =PRENDRE(TRIER($A$2:$J$319;10;-1;);10)
+    # # chiffres pour les 'titres de transport sociaux'
+    # # =PRENDRE(TRIER($A$2:$J$319;10;-1;);10)
     stations["M2"] = ArrayFormula("M2:V11",
-                                  "=_xlfn.TAKE(_xlfn.SORT($A$2:$J$319,10,-1,),10)")
+                                    "=_xlfn.TAKE(_xlfn.SORT($A$2:$J$319,10,-1,),10)")
     stations["M12"] = ArrayFormula("M12:V21",
-                                  "=_xlfn.TAKE(_xlfn.SORT($A$2:$J$319,10,-1,),-10)")
+                                    "=_xlfn.TAKE(_xlfn.SORT($A$2:$J$319,10,-1,),-10)")
     topflop(stations, 23)
 
-    ## chiffres pour les 'titres de transport courts'
+    # # chiffres pour les 'titres de transport courts'
     stations["Z2"] = ArrayFormula("Z2:AJ11",
-                                  "=_xlfn.TAKE(_xlfn.SORT($A$2:$K$319,11,-1,),10)")
+                                    "=_xlfn.TAKE(_xlfn.SORT($A$2:$K$319,11,-1,),10)")
     stations["Z12"] = ArrayFormula("Z12:AJ21",
-                                  "=_xlfn.TAKE(_xlfn.SORT($A$2:$K$319,11,-1,),-10)")
+                                    "=_xlfn.TAKE(_xlfn.SORT($A$2:$K$319,11,-1,),-10)")
     topflop(stations, 37)
 
-    # 6. feuille 'echantillon' : tirage aléatoire dynamique de 50 lignes de la feuille 'stations'
-    # en se basant sur les 50 premières valeurs de la colonne 'aleatoire'
+
+def echantillon(classeur: Workbook):
+    # crée la feuille 'echantillon'
+    # tirage aléatoire dynamique de 50 lignes de la feuille 'stations' en se basant
+    # sur les 50 premières valeurs de la colonne 'aleatoire' de cette feuille
     echantillon = classeur.create_sheet("echantillon")
-    ## entêtes
+
+    # # entêtes
     echantillon.append(["station",] + liste_titres)
 
-    ## =PRENDRE(TRIERPAR(stations!A:H;stations!I:I);50)
+    # # valeurs
+    # # =PRENDRE(TRIERPAR(stations!A:H;stations!I:I);50)
     classeur["echantillon"]["A2"] = ArrayFormula(
         'A2:H51', "=_xlfn.TAKE(_xlfn.SORTBY(stations!A:H,stations!I:I),50)")
+
+
+def feuilles(classeur: Workbook):
+    """
+    Orchestre le calcul des différentes feuilles de calcul
+    Plusieurs feuilles nécessitent la liste des différentes stations
+    et des différents titres de transport :
+    cette procédure initialise donc les variables globales liste_stations et liste_titres
+    :param classeur:
+    """
+    global liste_stations, liste_titres
+    liste_stations = unique(classeur["DATA"]["B"])
+    liste_titres = unique(classeur["DATA"]["C"])
+
+    # 1. feuille 'DATA' : ajout d'une colonne avec le numéro de la semaine
+    data(classeur)
+    # 2. feuille 'modalites'
+    modalites(classeur)
+    # 3. feuille 'jours'
+    jours(classeur)
+    # 4. feuille 'semaines'
+    semaines(classeur)
+    # 5. feuille 'stations'
+    stations(classeur)
+    # 6. feuille 'echantillon' : tirage aléatoire dynamique de 50 lignes de la feuille 'stations'
+    # en se basant sur les 50 premières valeurs de la colonne 'aleatoire' de la feuille 'stations'
+    echantillon(classeur)
